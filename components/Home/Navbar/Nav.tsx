5f5c448789baa9d27270c4cb906e037ba871e67a
PrionX-svg <oliviasalma06@gmail.com>
2025-02-02 00:06:30 +0700

    Add items into the navbar; logo and raw navigation links

--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
-
+import Image from 'next/image'
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <div className='fixed h-[12vh] z-[10] bg-blue-950 w-full'>
+      <div className='flex items-center h-full justify-between w-[95%] sm: w-[90%] xl: w-[80%] mx auto'>
+        {/* Logo */}
+        <Image src={'/images/logo.png'} alt={'Logo'}
+          width={55}
+          height={55}
+          className='ml-[-1.5rem] sm:ml-0'
+        />
+        {/* Navigation Links */}
+        <div className='flex items-center space-x-10'></div>
+      </div>
+    </div>
   )
 }
 
