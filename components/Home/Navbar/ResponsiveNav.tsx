620b222202255e3e591a8a9b062fbd8dea019adc
PrionX-svg <oliviasalma06@gmail.com>
2025-02-02 02:49:26 +0700

    Configure responsive navbar

--- components/Home/Navbar/ResponsiveNav.tsx
+++ components/Home/Navbar/ResponsiveNav.tsx
@@ -1,12 +1,21 @@
-import React from 'react'
+"use client";
+
+import React, { useState } from 'react'
 import Nav from './Nav'
 import MobileNav from './MobileNav'
 
 const ResponsiveNav = () => {
+  // Initialize state function
+  const [showNav, setShowNav] = useState(false);
+
+  // Function handler to show and close the navigation bar
+  const showNavHandler = () => setShowNav(true);
+  const closeNavHandler = () => setShowNav(false);
+
   return (
     <div>
-        <Nav/>
-        <MobileNav/>
+        <Nav openNavbar={showNavHandler}/>
+        <MobileNav showNav={showNav} closeNav={closeNavHandler}/>
     </div>
   )
 }
