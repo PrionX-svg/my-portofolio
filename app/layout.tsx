1954040d21245f9e899b996444081bad2a786464
PrionX-svg <oliviasalma06@gmail.com>
2025-02-01 23:26:04 +0700

    Add responsive navbar to the root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Sora } from "next/font/google";
 import "./globals.css";
+import ResponsiveNav from "@/components/Home/Navbar/ResponsiveNav";
 
 const font = Sora({
   weight: ['100', '200', '300', '400', '500', '600', '700', '800'],
@@ -12,16 +13,13 @@ export const metadata: Metadata = {
   description: "Website portofolio with TypeScript and Next js",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
+export default function RootLayout({children,}: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      <body
-        className={font.className}
-      >
+      <body className={font.className}>
+        <ResponsiveNav />
         {children}
       </body>
     </html>
